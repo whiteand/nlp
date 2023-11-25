74c1c2e15347bd0c5f4878b5138e32d585b9b4c1
whiteand <andrewbeletskiy@gmail.com>
2023-11-26 01:54:23 +0200

    feat: implemented mobile design

--- src/commands/serve/app/src/rootRoute.tsx
+++ src/commands/serve/app/src/rootRoute.tsx
@@ -10,7 +10,7 @@ function Root() {
   return (
     <>
       <Header />
-      <main className="mt-4 min-h-[calc(100vh_-_92px)]">
+      <main className="mt-4 min-h-[calc(100svh_-_180px)] md:min-h-[calc(100vh_-_92px)] px-3">
         <Outlet />
       </main>
       <Footer />
